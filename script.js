9258f2efee2b9b7b3458f28780daca7f6721035a
HaydenNease <hayden.nease@gmail.com>
2022-11-28 21:22:38 -0500

    addded confirm values

--- script.js
+++ script.js
@@ -3,6 +3,34 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // YOUR CODE GOES HERE
+  var isLowercase = confirm('Would you like to use lowercase?');
+  var isUppercase = confirm('Would you like to use uppercase?');  
+  var isNumber = confirm('Would you like to use numbers?');
+  var isSpecial = confirm('Would you like to use special characters?');
+
+  if (isLowercase) {
+    // USE LOWERCASE LETTERS
+    console.log('lowercase')
+  }
+
+  if (isUppercase) {
+    // USE UPPERCASE LETTERS
+    console.log('uppercase')
+  }  
+  
+  if (isNumber) {
+    // USE NUMBERS
+    console.log('number')
+  }
+
+  if (isSpecial) {
+    // USE SPECIAL CHARACTERS
+    console.log('special')
+  }
+
+  // GRAB RANDOM LETTERS
+
+  return "";
 }
 
 // Write password to the #password input
